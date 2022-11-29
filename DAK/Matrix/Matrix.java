efcaef0c0a7f6ed9ccf3549cb41306a4308d0c9c
mequam <blue9ja@gmail.com>
2022-11-29 12:16:56 -0500

    WIP commit got rid of the index error
    im not completely sure that this is working yet
    but at least its progres.

--- DAK/Matrix/Matrix.java
+++ DAK/Matrix/Matrix.java
@@ -9,11 +9,6 @@ class Matrix {
         
         Integer [] p = {5,7,2,3,7,4};
         PyramidArrayBack<Integer> order = chain_order(p);
-        order.forEach((x,y,data)->{
-            System.out.println(Integer.toString(x)+
-            ","+ Integer.toString(y)+
-            Integer.toString((Integer)data.get(x,y)));
-        });
         
     }
     
@@ -39,9 +34,9 @@ class Matrix {
     public static Integer compute_matrix_entry(int i,
         int j,
         int k,
-        PyramidArrayBack<Integer> m,Integer [] p)  {
-            
-        return m.get(k+1,j)+m.get(i,k)+p[i-1]*p[k]*p[j];
+        PyramidArrayBack<Integer> m,
+        Integer [] p)  {
+            return m.get(k+1,j) + m.get(i,k) + p[i]*p[k+1]*p[j+1];
     }
 
     /** 
@@ -49,45 +44,38 @@ class Matrix {
      * order for matrix multiplication.
     */
     public static PyramidArrayBack<Integer> chain_order(Integer [] p) {
-        PyramidArrayBack<Integer> ret_val = new PyramidArrayBack<Integer>(p.length);
-        PyramidArrayBack<Integer> reqs = new PyramidArrayBack<Integer>(p.length);
+        PyramidArrayBack<Integer> ret_val = new PyramidArrayBack<Integer>(p.length-1);
+        PyramidArrayBack<Integer> reqs = new PyramidArrayBack<Integer>(p.length-1);
 
         System.out.println(reqs.size());
-        System.out.println("printing data ---");
-        reqs.forEach((i,k,data)->{
-             System.out.println((Object)(Integer.toString(i) + "," + Integer.toString(k)));
+
+        reqs.forEachLong((i,k,data)->{
+            reqs.set(i, k, 0);
+        });
+        
+        reqs.forEachLong((i,j,data)->{
+            int max = Integer.MAX_VALUE;
+            int best_k = 0;
+
+            for (int k = i; k < j-1;k++) {
+                int challenger = compute_matrix_entry(i, j, k, reqs, p);
+                if (max > challenger) {
+                    max = challenger;
+                    best_k = k;
+                }
+            }
+        });
+        
+        reqs.forEachLong((i,k,data)->{
+            
         });
         System.out.println("finished printing data ---");
 
-        for (int i = 0; i < p.length;i++) {
-            System.out.println(Integer.toString(i)+","+ Integer.toString(i));
-            reqs.set(i, i, 0);
-        }
+        System.out.println("RUNNING MATRIX FOREACH");
         reqs.forEachLong((j,i,data)->{
-            int min = Integer.MAX_VALUE;  //give up a single integer value as infinity
-            int best_k = 1;
-            for (int k = i; k < j ; k++) {
-                int test = compute_matrix_entry(i,j,k,reqs,p);
-                
-                System.out.println(test);
-            
-                System.out.println(Integer.toString(i)+
-                    ","+ Integer.toString(j)+
-                    Integer.toString(test)
+            System.out.println(
+                Integer.toString(i) + ","  +Integer.toString(j)
                 );
-
-                if (test < min) {
-                   min = test;
-                   best_k = k;
-                }
-            }
-            ret_val.set(i, j, best_k);
-            reqs.set(i, j, min); 
-            System.out.println(Integer.toString(i)+
-                ","+ Integer.toString(j)+
-                Integer.toString(min)
-            );
-
         }); 
         return ret_val;
     }
